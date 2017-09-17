4f45e8ac5d498c99a8c1b2ea021e8d91a62c9d8e
chanheepark <ch13p@naver.com>
2017-09-17 17:37:20 +0900

    에코

--- src/Program.java
+++ src/Program.java
@@ -1,44 +1,71 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Scanner;
-
 public class Program {
-    public static void main(String[] args) {
-        InetSocketAddress socketAddress = new InetSocketAddress("192.168.11.26", 6000);
-        Socket socket = new Socket();
-        try {
-            socket.connect(socketAddress);
-            System.out.println("good");
-
-            Scanner scanner = new Scanner(System.in);
-            byte[] data = new byte[1024];
-
-            while (true) {
-                InputStream is = socket.getInputStream();
-                OutputStream os = socket.getOutputStream();
-
-                if (!scanner.hasNext()) {
-                    break;
-                }
-
-                String line = scanner.next();
-                os.write(line.getBytes());
-
-                int len = is.read(data);
-                if (len == -1) {
-                    break;
-                }
-
-                String str = new String(data, 0, len);
-                System.out.println("form Server : " + str);
-            }
-
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String args[]) {
+        if (args.length != 1) {
+            System.out.println("USAGE: java TcpIpMultichatClient 대화명");
+            System.exit(0);
         }
+        try {
+            String serverIp = "127.0.0.1";
+            // 소켓을 생성하여 연결을 요청한다.
+            Socket socket = new Socket(serverIp, 7777);
+            System.out.println("서버에 연결되었습니다.");
+            Thread sender = new Thread(new ClientSender(socket, args[0]));
+            Thread receiver = new Thread(new ClientReceiver(socket));
+            sender.start();
+            receiver.start();
+        } catch (ConnectException ce) {
+            ce.printStackTrace();
+        } catch (Exception e) {
+        }
+    } // main
+    static class ClientSender extends Thread {
+        Socket socket;
+        DataOutputStream out;
+        String name;
+        ClientSender(Socket socket, String name) {
+            this.socket = socket;
+            try {
+                out = new DataOutputStream(socket.getOutputStream());
+                this.name = name;
+            } catch (Exception e) {
+            }
+        }
+        public void run() {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                if (out != null) {
+                    out.writeUTF(name);
+                }
+                while (out != null) {
+                    out.writeUTF("[" + name + "]" + scanner.nextLine());
+                }
+            } catch (IOException e) {
+            }
+        } // run()
+    }
+    static class ClientReceiver extends Thread {
+        Socket socket;
+        DataInputStream in;
+        ClientReceiver(Socket socket) {
+            this.socket = socket;
+            try {
+                in = new DataInputStream(socket.getInputStream());
+            } catch (IOException e) {
+            }
+        }
+        public void run() {
+            while (in != null) {
+                try {
+                    System.out.println(in.readUTF());
+                } catch (IOException e) {
+                }
+            }
+        } // run
     }
 }
