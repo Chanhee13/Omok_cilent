dfd4723b9ce51d151cee506e444ccb4116bb7608
chanheepark <ch13p@naver.com>
2017-09-17 18:07:55 +0900

    에코

--- src/Program.java
+++ src/Program.java
@@ -6,19 +6,15 @@ import java.net.Socket;
 import java.util.Scanner;
 public class Program {
     public static void main(String args[]) {
-        if (args.length != 1) {
-            System.out.println("USAGE: java TcpIpMultichatClient 대화명");
-            System.exit(0);
-        }
         try {
-            String serverIp = "127.0.0.1";
-            // 소켓을 생성하여 연결을 요청한다.
+            String serverIp = "192.168.11.189";
             Socket socket = new Socket(serverIp, 7777);
             System.out.println("서버에 연결되었습니다.");
-            Thread sender = new Thread(new ClientSender(socket, args[0]));
+            Thread sender = new Thread(new ClientSender(socket, "찬희"));
             Thread receiver = new Thread(new ClientReceiver(socket));
             sender.start();
             receiver.start();
+
         } catch (ConnectException ce) {
             ce.printStackTrace();
         } catch (Exception e) {
@@ -47,7 +43,7 @@ public class Program {
                 }
             } catch (IOException e) {
             }
-        } // run()
+        }
     }
     static class ClientReceiver extends Thread {
         Socket socket;
@@ -66,6 +62,6 @@ public class Program {
                 } catch (IOException e) {
                 }
             }
-        } // run
+        }
     }
 }
